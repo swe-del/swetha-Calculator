065e091ba5d410b90887445970a2c326cff3cedf
swetha prakasam <swethaprakasamkittu@gmail.com>
2023-09-20 13:18:49 +0530

    added files

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from math import sin,tan,cos,sqrt
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def calculator():
+    if request.method == "POST":
+        expression = request.form.get("expression")
+        try:
+            result = eval(expression)
+        except:
+            result = "Invalid expression"
+        return render_template("cal.html", expression=expression, result=result)
+    return render_template("cal.html")
+
+if __name__ == "__main__":
+    app.run(port=5000)
